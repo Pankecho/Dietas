6e4d0cbf34025721cfce641ea6c41ae6dfc363e0
Eduardo Venegas <vese94@gmail.com>
2017-11-12 11:36:58 -0600

    GUI2

--- src/Interfaz/Menu.java
+++ src/Interfaz/Menu.java
@@ -24,6 +24,7 @@ public class Menu extends javax.swing.JFrame {
     panelEjercicio panelEjercicio= new panelEjercicio();
     public Menu() {
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     /**
